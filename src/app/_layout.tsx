241b6fdde5bf2387d3f4e06d72a49743b4c8b59d
MatiasOjman <167230970+MatiasOjman@users.noreply.github.com>
2026-06-02 08:13:42 -0300

    archivos creados

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,14 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import { useColorScheme } from 'react-native';
+import { Stack } from "expo-router";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+export default function RootLayout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="crear-cuenta" />
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="agregar-medicacion" />
+      <Stack.Screen name="agregar-actividad" />
+      <Stack.Screen name="detalle-dia" />
+    </Stack>
   );
 }
